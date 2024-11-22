848eeddc0c33702801bb8f291abfb4da56b6a122
Karan kendre <142140048+kendrekaran@users.noreply.github.com>
2024-11-23 00:39:21 +0530

    Update App.jsx

--- Soft-main/src/App.jsx
+++ Soft-main/src/App.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter and other routing components
+import React from 'react';// Import BrowserRouter and other routing components
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Project'
@@ -9,18 +8,15 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    
       <div className="text-gray-200">
-        <Routes>
-       
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
       </div>
-    </Router>
+    
   );
 }
 
